328c8c2f659ff605cffdf664ca057ae6e8dd0eac
Vishal Nirmal <vishalnirmal53@gmail.com>
2021-02-11 17:45:26 +0530

    Changed the data structure use for storing tasks

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ class task {
     constructor(text, completed = false) {
         this.text = text;
         this.completed = completed;
+        this.id = (Math.floor(Math.random()*1000000000)).toString();
     }
 }
 var filters = [document.querySelectorAll(".all"), document.querySelectorAll(".active"), document.querySelectorAll(".completed")];
@@ -10,7 +11,7 @@ var task_format_1 = '<div class=\"item ';
 var task_format_2 = '\" id=\"';
 var task_format_3 = '\"><span class=\"check\"></span><p class=\"text\">';
 var task_format_4 = '</p><span class=\"delete\"></span></div>';
-var tasks = {};
+var tasks = [];
 if (localStorage.getItem("tasks")){
     tasks = JSON.parse(localStorage.getItem("tasks"));
     document.querySelector(".counter span").textContent = countRemaining();
@@ -18,8 +19,7 @@ if (localStorage.getItem("tasks")){
 }
 document.querySelector(".form").addEventListener("submit", (e)=>{
     e.preventDefault();
-    var id = (Math.floor(Math.random()*1000000000)).toString();
-    tasks[id] = new task(e.target.task.value);
+    tasks.push(new task(e.target.task.value))
     e.target.task.value = "";
     localStorage.setItem("tasks", JSON.stringify(tasks));
     filterSelection(0);
@@ -27,10 +27,10 @@ document.querySelector(".form").addEventListener("submit", (e)=>{
 });
 function countRemaining(){
     let i = 0;
-    for (key in tasks){
-        if(!tasks[key].completed)
-        i++;
-    }
+    tasks.forEach(task=>{
+        if(!task.completed)
+            i++;
+    })
     return i;
 }
 function filterSelection(index){
@@ -45,46 +45,52 @@ function filterSelection(index){
     });
 }
 function showTasks(tasks){
-    var temp = {};
+    var temp = [];
     if(selected[0]){
         temp = tasks;
     }
     else if(selected[1]){
-        for (key in tasks){
-            if(!tasks[key].completed)
-                temp[key] = tasks[key];
-        }
+        tasks.forEach(task =>{
+            if(!task.completed)
+                temp.push(task);
+        });
     }
     else{
-        for (key in tasks){
-            if(tasks[key].completed)
-                temp[key] = tasks[key];
-        }
+        tasks.forEach(task =>{
+            if(task.completed)
+                temp.push(task);
+        });
     }
     var data = "";
-    for(key in temp)
-        data = data + task_format_1 + (tasks[key].completed?"completed":"") + task_format_2 + key + task_format_3 + tasks[key].text + task_format_4;
+    temp.forEach(task=>{
+        data = data + task_format_1 + (task.completed?"completed":"") + task_format_2 + task.id + task_format_3 + task.text + task_format_4;
+    }); 
     document.querySelector(".tasks").innerHTML = data;
     var check_button = document.querySelectorAll(".tasks .item .check");
     var delete_button = document.querySelectorAll(".tasks .item .delete");
     check_button.forEach(button => {
         button.addEventListener("click", ()=>{
             var id = button.parentElement.id;
-            if(tasks[id].completed){
-                button.parentElement.classList.remove("completed");
+            for(let num=0;num<tasks.length;num++){
+                if(tasks[num].id == id){
+                    if(tasks[num].completed){
+                        button.parentElement.classList.remove("completed");
+                    }
+                    else{
+                        button.parentElement.classList.add("completed");
+                    }
+                    tasks[num].completed = !tasks[num].completed;
+                    document.querySelector(".counter span").textContent = countRemaining();
+                    localStorage.setItem("tasks", JSON.stringify(tasks));
+                    break;
+                }
             }
-            else{
-                button.parentElement.classList.add("completed");
-            }
-            tasks[id].completed = !tasks[id].completed;
-            document.querySelector(".counter span").textContent = countRemaining();
-            localStorage.setItem("tasks", JSON.stringify(tasks));
         });
     });
     delete_button.forEach(button =>{
         button.addEventListener("click", ()=>{
             let id = button.parentElement.id;
-            delete tasks[id];
+            tasks = tasks.filter(task => task.id != id);
             localStorage.setItem("tasks", JSON.stringify(tasks));
             showTasks(tasks);
         });
@@ -92,10 +98,7 @@ function showTasks(tasks){
     document.querySelector(".counter span").textContent = countRemaining();
 }
 document.querySelector(".clear").addEventListener("click", ()=>{
-    for (key in tasks){
-        if(tasks[key].completed)
-            delete tasks[key];
-    }
+    tasks = tasks.filter(task => !task.completed)
     localStorage.setItem("tasks", JSON.stringify(tasks));
     showTasks(tasks);
 });
